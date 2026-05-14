fa692f24cd490d21f89d4c819326f4ea3c9b0fa7
Rina Meisenhelter <rina.meisenhelter@smail.th-koeln.de>
2026-05-14 20:15:10 +0200

    Clean up MainActivity and remove boilerplate
    - Remove unused Compose imports.
    - Delete commented-out template code from the default project activity.

--- app/src/main/java/com/example/smartmeetup/MainActivity.kt
+++ app/src/main/java/com/example/smartmeetup/MainActivity.kt
@@ -6,13 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.smartmeetup.ui.app.SmartMeetupApp
 import com.example.smartmeetup.ui.theme.SmartMeetUpTheme
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,38 +19,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-// Following Code ist Standard-Template-Code von Android Studio
-/*
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            SmartMeetUpTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartMeetUpTheme {
-        Greeting("Android")
-    }
-}
- */
