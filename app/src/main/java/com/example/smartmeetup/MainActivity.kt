1a6edebe1ada16f8c17652dfa891ccceb33cc6fa
Rina Meisenhelter <rina.meisenhelter@smail.th-koeln.de>
2026-06-10 17:55:37 +0900

    Initialize OsmDroid and update Gradle configuration

--- app/src/main/java/com/example/smartmeetup/MainActivity.kt
+++ app/src/main/java/com/example/smartmeetup/MainActivity.kt
@@ -30,6 +30,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        OsmDroidConfig.initialize(applicationContext)
+
         enableEdgeToEdge()
 
         setContent {
